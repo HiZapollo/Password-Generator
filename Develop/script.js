d8ccdddb66f54f1641213752990ec8d13602bc09
zeckenrode <zeckenrode9000@gmail.com>
2023-05-29 01:46:35 -0400

    Added the generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,40 @@
-// Assignment code here
+//Assignment code here
 
+var letters = "abcdefghijklmnopqrstuvwxyz"
+var numbers = "0123456789"
+var specials = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+
+function generatePassword(){
+  var length = prompt("Enter a password length between 8 and 128 characters: ")
+  var haveUpper = confirm("Include Uppercase Letters?")
+  var haveLower = confirm("Include Lowercase Letters?")
+  var haveNum = confirm("Include Numbers?")
+  var haveSpecial = confirm("Include Special Characters?")
+
+  var password = ''
+  var choices = ''
+  if (length < 8){
+    length = 8
+  } if (length > 128){
+    length = 128
+  } if(haveLower){
+    choices += letters
+  } if (haveUpper){
+    choices += letters.toUpperCase()
+  } if (haveNum){
+    choices += numbers
+  } if (haveSpecial){
+    choices += specials
+  } if (!haveLower && !haveUpper && !haveNum && !haveSpecial){
+    choices += letters
+  }
+  
+  
+  while (password.length < length){
+    password += choices.charAt(Math.floor(Math.random() * choices.length));
+  }
+  return password;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
